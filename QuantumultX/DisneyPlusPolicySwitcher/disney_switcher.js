aa40dd8b74712dcb8e54dfd6e88bef78a2694b7f
xulizhe666 <68450475+xulizhe666@users.noreply.github.com>
2022-03-23 13:40:26 +0800

    Update disney_switcher.js
    去掉通知

--- QuantumultX/DisneyPlusPolicySwitcher/disney_switcher.js
+++ QuantumultX/DisneyPlusPolicySwitcher/disney_switcher.js
@@ -34,7 +34,7 @@ let concurrency = parseInt($.getval('Helge_0x00.Disney_Concurrency')) || 10
   if (status === STATUS_AVAILABLE) {
     let flag = getCountryFlagEmoji(region) ?? ''
     let regionName = REGIONS?.[region.toUpperCase()]?.chinese ?? ''
-    $.msg($.name, `${actualNode}`, `当前节点支持 Disney+ ➟ ${flag} ${regionName}`)
+    $.msge($.name, `${actualNode}`, `当前节点支持 Disney+ ➟ ${flag} ${regionName}`)
     return
   }
 
@@ -77,12 +77,12 @@ let concurrency = parseInt($.getval('Helge_0x00.Disney_Concurrency')) || 10
   let flag = getCountryFlagEmoji(newRegion) ?? ''
   let regionName = REGIONS?.[newRegion.toUpperCase()]?.chinese ?? ''
   console.log(`\n切换策略：${curPolicyPath.join(' ➤ ')} ➟ ${switchPath.join(' ➤ ')}`)
-  $.msg($.name, `${curPolicyPath[curPolicyPath.length - 1]} ➟ ${switchPath[switchPath.length - 1]}`, `该节点支持 Disney+ ➟ ${flag} ${regionName}`)
+  $.msge($.name, `${curPolicyPath[curPolicyPath.length - 1]} ➟ ${switchPath[switchPath.length - 1]}`, `该节点支持 Disney+ ➟ ${flag} ${regionName}`)
 })()
   .catch(error => {
     console.log(error)
     if (typeof error === 'string') {
-      $.msg($.name, '', `${error} ⚠️`)
+      $.msge($.name, '', `${error} ⚠️`)
     }
   })
   .finally(() => {
